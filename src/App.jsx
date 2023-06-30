3b5a009bd5aacbe8238d5af2213386c7cabce689
Eszuri <ualdo296@gmail.com>
2023-06-30 08:54:29 +0700

    222

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import App2 from "./App2"
-import App3 from "./App3"
+// import App2 from "./App2"
+// import App3 from "./App3"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
 
+function About() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>About View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>404: Page Not Found</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App2 />} />
-          <Route path="/k" element={<App3 dd="direcK" />} />
-          <Route path="*" element={<App3 />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/qw" element={<About />} />
+          <Route path="*" element={<NoMatch />} />
         </Routes>
       </BrowserRouter>
 
